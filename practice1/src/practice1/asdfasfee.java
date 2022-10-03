42c76db7fc777286c3ea055d01dd93cb04f3d40a
kjw <kjw@DESKTOP-CSA8PVS>
2022-10-03 18:15:23 +0900

    asdf

---
+++ practice1/src/practice1/asdfasfee.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class asdfasfee {
+
+	public static void main(String[] args) {
+		// name, age, height
+		System.out.println("kjw");
+		int x = 26;
+		System.out.println(x);
+		int y = 175;
+		System.out.println(y);
+	}
+
+}
